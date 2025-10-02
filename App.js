95dab23a4f51b3fb3ff51606f51474b32c0933d0
shubham11408139 <shubhamgupta.608@rediffmail.com>
2025-10-02 23:35:06 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 /* <div id ="parent">
   <div id="child">
     <h1>Shubham</h1>
@@ -15,7 +16,7 @@ const parent = React.createElement("div", {id:"parent"},
     [React.createElement("div", {id:"child"},[
         React.createElement("h1", {},"Shubham"),
         React.createElement("h1", {},"AKSHAY")
-    ]),
+    ]),  
      React.createElement("div", {id:"child2"},[
         React.createElement("h1", {},"Shubham2"),
         React.createElement("h1", {},"AKSHAY2")
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Namate React
+# Namaste React
+# Parcel
+- Dev build
+- Local server 
+- HMR(HOT MODULE REPLACEMENT)
+- File wacthing Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling 
+- Compress of the files
+- Consitent hashing
+- Code Splitting
+- Differential Bundling - upport older broswers
+- Diagnostic
+- Error Handling
+- HTTPs
