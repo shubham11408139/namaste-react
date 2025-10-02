9c6fa85a7507e7dc370bb5426dd2178eec292723
shubham11408139 <shubhamgupta.608@rediffmail.com>
2025-10-02 20:50:24 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,29 @@
+
+/* <div id ="parent">
+  <div id="child">
+    <h1>Shubham</h1>
+    <h1>AKSHAY</h1>
+  </div>
+  <div id="child2">
+    <h1>Shubham2</h1>
+    <h1>AKSHAY2</h1>
+  </div>
+</div> */
+
+
+const parent = React.createElement("div", {id:"parent"},
+    [React.createElement("div", {id:"child"},[
+        React.createElement("h1", {},"Shubham"),
+        React.createElement("h1", {},"AKSHAY")
+    ]),
+     React.createElement("div", {id:"child2"},[
+        React.createElement("h1", {},"Shubham2"),
+        React.createElement("h1", {},"AKSHAY2")
+    ])]
+)
+
+// const heading = React.createElement("h1", {id:"heading", xyz:"abc"}, "Hello World from React!");
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ 
+root.render(parent);
